0312fdef3ff344727af0f30082f8b037a47ba0ed
Muhammad Ali <67803352+FunBirdUz@users.noreply.github.com>
2021-01-14 23:14:09 +0500

    config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
-bot_token = "1488727769:AAElRU7yS309GP1t54F635ASDF"
+bot_token = "1541358509:AAHSODIaCHc3nFtX075XUiqGOkNZywNXQ6c"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001485393652
-owner_id = 1243703097
+sudo_chat_id = -1001447818514
+owner_id = 352040593
