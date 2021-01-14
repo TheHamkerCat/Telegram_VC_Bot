651c9a495bdd83d42c319142cc0515a6d38088fa
Muhammad Ali <67803352+FunBirdUz@users.noreply.github.com>
2021-01-14 21:36:22 +0500

    config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
 jio_saavn_api = "http://127.0.0.1:5000/result/?query="
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001485393652
-owner_id = 1243703097
+sudo_chat_id = -1001447818514
+owner_id = 352040593
