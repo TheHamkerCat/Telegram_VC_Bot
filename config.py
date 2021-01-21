f25a75c8394a1901f7d22d7a63b655796f4b54d3
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2021-01-21 19:05:40 +0530

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+bot_token = "1482166651:AAGZq_Qyz6Sm08ChGme4kcR4E6unr4pR144"
+radio_link = "http://peridot.streamguys.com:7150/Mirchi"
+sudo_chat_id = -1001338098714
+owner_id = 1388895264
--- sample_config.py
+++
@@ -1,4 +0,0 @@
-bot_token = "1488727769:AAElRU7yS309GP1t54F635ASDF"
-radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001485393652
-owner_id = 1243703097
