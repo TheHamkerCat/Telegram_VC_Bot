66ea48b2a5b3539a5c0dcd0446bfe2c305de94cb
TheHamkerCat <thehamkercat@gmail.com>
2021-02-02 11:32:11 +0530

    The return of clean up shit

---
+++ functions.py
@@ -0,0 +1,146 @@
+import os
+import aiohttp
+import aiofiles
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+
+# Os Determination
+if os.name == "nt":
+    kill = "tskill"
+else:
+    kill = "killall -9"
+
+
+# Get User Input
+def kwairi(message):
+    query = ""
+    for i in message.command[1:]:
+        query += f"{i} "
+    return query
+
+
+# Convert seconds to mm:ss
+def convert_seconds(seconds):
+    seconds = seconds % (24 * 3600)
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return "%02d:%02d" % (minutes, seconds)
+
+
+# Convert hh:mm:ss to seconds
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
+
+
+# Change image size
+def changeImageSize(maxWidth, maxHeight, image):
+    widthRatio = maxWidth / image.size[0]
+    heightRatio = maxHeight / image.size[1]
+    newWidth = int(widthRatio * image.size[0])
+    newHeight = int(heightRatio * image.size[1])
+    newImage = image.resize((newWidth, newHeight))
+    return newImage
+
+
+# Prepare for next song
+async def prepare(s, m, message):
+    try:
+        os.system(f"{kill} mpv")
+    except:
+        pass
+    try:
+        await m.delete()
+    except:
+        pass
+    try:
+        await message.delete()
+    except:
+        pass
+    try:
+        os.remove("audio.webm")
+    except:
+        pass
+    try:
+        os.remove("downloads/audio.webm")
+    except:
+        pass
+    try:
+        s.terminate()
+    except:
+        pass
+
+
+# Generate cover for jiosaavn and deezer
+async def generate_cover_square(message, title, artist, duration, thumbnail):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(thumbnail) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open("background.png", mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+    image1 = Image.open("./background.png")
+    image2 = Image.open("etc/foreground_square.png")
+    image3 = changeImageSize(600, 500, image1)
+    image4 = changeImageSize(600, 500, image2)
+    image5 = image3.convert("RGBA")
+    image6 = image4.convert("RGBA")
+    Image.alpha_composite(image5, image6).save("temp.png")
+    img = Image.open("temp.png")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("etc/font.otf", 20)
+    draw.text((150, 380), f"Title: {title}", (255, 255, 255), font=font)
+    draw.text((150, 405), f"Artist: {artist}", (255, 255, 255), font=font)
+    draw.text(
+        (150, 430),
+        f"Duration: {duration} Seconds",
+        (255, 255, 255),
+        font=font,
+    )
+    draw.text(
+        (150, 455),
+        f"Played By: {message.from_user.first_name}",
+        (255, 255, 255),
+        font=font,
+    )
+    img.save("final.png")
+    os.remove("temp.png")
+    os.remove("background.png")
+
+
+# Generate cover for youtube
+async def generate_cover(message, title, views, duration, thumbnail):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(thumbnail) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open("background.png", mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+
+    image1 = Image.open("./background.png")
+    image2 = Image.open("etc/foreground.png")
+    image3 = changeImageSize(1280, 720, image1)
+    image4 = changeImageSize(1280, 720, image2)
+    image5 = image3.convert("RGBA")
+    image6 = image4.convert("RGBA")
+    Image.alpha_composite(image5, image6).save("temp.png")
+    img = Image.open("temp.png")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("etc/font.otf", 32)
+    draw.text((190, 550), f"Title: {title}", (255, 255, 255), font=font)
+    draw.text(
+        (190, 590), f"Duration: {duration}", (255, 255, 255), font=font
+    )
+    draw.text((190, 630), f"Views: {views}", (255, 255, 255), font=font)
+    draw.text(
+        (190, 670),
+        f"Played By: {message.from_user.first_name}",
+        (255, 255, 255),
+        font=font,
+    )
+    img.save("final.png")
+    os.remove("temp.png")
+    os.remove("background.png")
