650e511494abf7cacba9935d05927e87bc80ed4c
TheHamkerCat <thehamkercat@gmail.com>
2021-03-17 01:49:30 +0530

    Cleaned shit

--- misc.py
+++ misc.py
@@ -1,20 +1,11 @@
 HELP_TEXT = """**Currently These Commands Are Supported.**
 /start To Start The bot.
 /help To Show This Message.
-/ping To Ping All Datacenters Of Telegram.
 /skip To Skip The Current Playing Music.
 /play youtube/saavn/deezer [song_name]
 /joinvc [CHAT_ID] To Join A Voice Chat.
 /leavevc [CHAT_ID] To Leave A Voice Chat.
-/listvc To List Vcs
 /telegram While Taging a Song To Play From Telegram File.
-/users To Get A List Of Blacklisted Users.
-
-**Admin Commands**:
-/authorize To Authorize A Chat.
-/unauthorize TO Unauthorize A Chat.
-/black To Blacklist A User.
-/white To Whitelist A User.
 
 NOTE: Do Not Assign These Commands To Bot Via BotFather"""
 
