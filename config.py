486971f6b4f0744b99a6d3e5d7ad88e47c3a41be
TheProgrammerCat <sdevanagaraj@gmail.com>
2021-03-20 15:31:19 +0530

    Update and rename sample_config.py to config.py
    I still hate it

---
+++ config.py
@@ -0,0 +1,8 @@
+api_id = int(os.getenv("Api ID"))
+api_hash = os.getenv("Api Hash")
+sudo_chat_id = int(os.getenv("Chat ID"))
+owner_id = int(os.getenv("Owner ID"))
+
+
+# Arq Api, don't make changes here
+ARQ_API = "https://thearq.tech"
--- sample_config.py
+++
@@ -1,8 +0,0 @@
-api_id = 14371
-api_hash = "e46b6c854d2bf58a0"
-sudo_chat_id = -1001485876964
-owner_id = 1243703097
-
-
-# Arq Api, don't make changes here
-ARQ_API = "https://thearq.tech"
