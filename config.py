1f4bae0205e7b9a95c9db577c38a10adb0371cf1
TheProgrammerCat <sdevanagaraj@gmail.com>
2021-03-21 20:12:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 
 class Config(object):
-  API_ID = os.getenv("Api ID"))
+  API_ID = int(os.getenv("Api ID"))
   API_HASH = os.getenv("Api Hash")
-  SUDO_CHAT_ID = os.getenv("Chat ID"))
-  OWNER_ID = os.getenv("Owner ID"))
+  SUDO_CHAT_ID = int(os.getenv("Chat ID"))
+  OWNER_ID = int(os.getenv("Owner ID"))
 
 
 # Arq Api, don't make changes here
