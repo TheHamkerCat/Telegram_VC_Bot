9fba422a2b6d4d1758226554a593c7c0d8b2f496
TheHamkerCat <thehamkercat@gmail.com>
2021-04-11 03:10:41 +0530

    Added features | Fixed things.

--- sample_config.py
+++ sample_config.py
@@ -1,21 +1,7 @@
-HEROKU = True   # NOTE Make it false if you're not deploying on heroku.
-
-# NOTE these values are for heroku.
-if HEROKU:
-    from os import environ
-    API_ID = int(environ["API_ID"])
-    API_HASH = environ["API_HASH"]
-    SUDO_CHAT_ID = int(environ["SUDO_CHAT_ID"])
-    OWNER_ID = int(environ["OWNER_ID"])
-    SESSION_STRING = environ["SESSION_STRING"]
-
-# NOTE Fill this if you are not deploying on heroku.
-if not HEROKU:
-    API_ID = 14371
-    API_HASH = "e46b6c854d2bf58a0"
-    SUDO_CHAT_ID = -1001485876964
-    OWNER_ID = 1243703097
-
+API_ID = 14371
+API_HASH = "e46b6c854d2bf58a0"
+SUDO_CHAT_ID = -1001485876964  # Chat where the bot will play the music.
+SUDOERS = [1243703097, 13216546] # Users which have special control over the bot.
 
 # don't make changes below this line
 ARQ_API = "http://35.240.133.234:8000"
