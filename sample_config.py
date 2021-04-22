badc9baea61f4732d9131ea641795326bf5269d8
Tejas61900 <73832085+Tejas61900@users.noreply.github.com>
2021-04-22 19:38:12 +0530

    Fixed Heroku (#47)
    * Update sample_config.py
    
    * Update main.py
    
    * Update app.json

--- sample_config.py
+++ sample_config.py
@@ -1,10 +1,20 @@
-from os import environ
+HEROKU = True   # NOTE Make it false if you're not deploying on heroku.
 
-API_ID = 14371
-API_HASH = "e46b6c854d2bf58a0"
-SUDO_CHAT_ID = -1001485876964  # Chat where the bot will play the music.
-SUDOERS = [1243703097, 13216546] # Users which have special control over the bot.
-SESSION_STRING = "sgjiidhehje:jkeooh" #Check Readme for session
+# NOTE these values are for heroku.
+if HEROKU:
+    from os import environ
+    API_ID = int(environ["API_ID"])
+    API_HASH = environ["API_HASH"]
+    SUDO_CHAT_ID = int(environ["SUDO_CHAT_ID"]) # Chat where the bot will play the music.
+    SUDOERS = int(environ["SUDOERS"]) # Users which have special control over the bot.
+    SESSION_STRING = environ["SESSION_STRING"] # Check Readme for session
+
+# NOTE Fill this if you are not deploying on heroku.
+if not HEROKU:
+    API_ID = 14371
+    API_HASH = "e46b6c854d2bf58a0"
+    SUDO_CHAT_ID = -1001485876964
+    SUDOERS = [1243703097, 13216546]
 
 # don't make changes below this line
 ARQ_API = "https://thearq.tech"
