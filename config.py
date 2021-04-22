3b6cdf80ab8d845d51d8be0e52d6e1eaf6886f9f
Tejas61900 <73832085+Tejas61900@users.noreply.github.com>
2021-04-22 17:03:41 +0530

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ API_HASH = "e46b6c854d2bf58a0"
 SUDO_CHAT_ID = -1001485876964  # Chat where the bot will play the music.
 SUDOERS = [1243703097, 13216546] # Users which have special control over the bot.
 SESSION_STRING = "sgjiidhehje:jkeooh" #Check Readme for session
+
 # don't make changes below this line
 ARQ_API = "https://thearq.tech"
