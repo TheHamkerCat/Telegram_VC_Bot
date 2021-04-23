ad74b5e1a195b13b6f21e53064213f32c1d287fb
Tejas61900 <73832085+Tejas61900@users.noreply.github.com>
2021-04-23 09:23:10 +0530

    Fixed Sudoers For Heroku (#49)
    * Update sample_config.py
    
    * Update app.json
    
    * Update app.json

--- sample_config.py
+++ sample_config.py
@@ -6,7 +6,7 @@ if HEROKU:
     API_ID = int(environ["API_ID"])
     API_HASH = environ["API_HASH"]
     SUDO_CHAT_ID = int(environ["SUDO_CHAT_ID"]) # Chat where the bot will play the music.
-    SUDOERS = int(environ["SUDOERS"]) # Users which have special control over the bot.
+    SUDOERS = list(int(x) for x in environ.get("SUDOERS", "").split()) # Users which have special control over the bot.
     SESSION_STRING = environ["SESSION_STRING"] # Check Readme for session
 
 # NOTE Fill this if you are not deploying on heroku.
