103d1095e674ed7700a4544581d1b09639b539e5
TheHamkerCat <thehamkercat@gmail.com>
2021-05-15 03:49:27 +0530

    Api key

--- sample_config.py
+++ sample_config.py
@@ -8,6 +8,7 @@ if HEROKU:
     SUDO_CHAT_ID = int(environ["SUDO_CHAT_ID"]) # Chat where the bot will play the music.
     SUDOERS = list(int(x) for x in environ.get("SUDOERS", "").split()) # Users which have special control over the bot.
     SESSION_STRING = environ["SESSION_STRING"] # Check Readme for session
+    ARQ_API_KEY = environ['ARQ_API_KEY']
 
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
@@ -15,6 +16,6 @@ if not HEROKU:
     API_HASH = "e46b6c854d2bf58a0"
     SUDO_CHAT_ID = -1001485876964
     SUDOERS = [1243703097, 13216546]
-
+    ARQ_API_KEY = "Get this from @ARQRobot"
 # don't make changes below this line
 ARQ_API = "https://thearq.tech"
