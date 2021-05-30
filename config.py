f27bb39e5cc2a29f40ff5d0cb192014814608387
sangramghangale47 <80095122+sangramghangale47@users.noreply.github.com>
2021-05-30 09:42:39 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+ API_ID = "3332133"
+    API_HASH = "da344e8da9287ac663705b5ab22ba65d"
+    ARQ_API_KEY = "Get this from @ARQRobot"
+# don't make changes below this line
+ARQ_API = "https://thearq.tech"
