6a3d323e20c5b5c317f08fd7ce961b2c00bf49a1
Tejas61900 <73832085+Tejas61900@users.noreply.github.com>
2021-06-03 11:34:10 +0530

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Only for Docker
+# Setting env values with the help of bash script
+# Edit following values
+
+API_ID='69'
+
+API_HASH='e46b6c854d2bf58a0'
+
+SESSION_STRING='your_string'
+
+ARQ_API_KEY='Get this from @ARQRobot'
+
+ARQ_API='https://thearq.tech'
