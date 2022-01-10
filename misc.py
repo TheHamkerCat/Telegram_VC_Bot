5fd0f93b437f4d7a6b128bd76d45e7166a10f058
https://t.me/meysam_khoban <96869864+Meysamm12@users.noreply.github.com>
2022-01-10 08:56:07 +0330

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,18 +1,18 @@
 HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
 
-**/skip** __Skip The Current Playing Music.__
-**/play** __Service_Or_Default (Services: youtube/saavn, Default: youtube) Song_Name | Reply_To_Audio__
-**/joinvc** __Join Voice Chat.__
-**/leavevc** __Leave Voice Chat.__
-**/volume [1-200]** __Adjust Volume.__
-**/pause** __Pause Music.__
-**/resume** __Resume Music.__
-**/queue plformat/Nothing** __Shows Queue List. If you send plformat with command you will get it in playlist format.__
-**/delqueue** __Deletes Queue List and Playlist.__
-**/playlist** __Start Playing Playlist.__
+**skip** __Skip The Current Playing Music.__
+**play** __Service_Or_Default (Services: youtube/saavn, Default: youtube) Song_Name | Reply_To_Audio__
+**joinvc** __Join Voice Chat.__
+**leavevc** __Leave Voice Chat.__
+**volume [1-200]** __Adjust Volume.__
+**pause** __Pause Music.__
+**resume** __Resume Music.__
+**queue plformat/Nothing** __Shows Queue List. If you send plformat with command you will get it in playlist format.__
+**delqueue** __Deletes Queue List and Playlist.__
+**playlist** __Start Playing Playlist.__
 """
 
 REPO_TEXT = (
-    "[Github](https://github.com/thehamkercat/Telegram_vc_bot)"
-    + " | [Group](t.me/TGVCSUPPORT)"
+    "[Github](https://Meysamm12/Telegram_VC_Bot)"
+    + " | [Group](t.me/meysam_khoban)"
 )
